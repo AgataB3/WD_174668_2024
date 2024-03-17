525ac38df99e9e7b57dce5fd55d9f48603d701cc
AgataB3 <147510880+AgataB3@users.noreply.github.com>
2024-03-17 15:33:24 +0100

    Update wd3_lab3_pdf.py

--- wd3_lab3_pdf.py
+++ wd3_lab3_pdf.py
@@ -28,7 +28,7 @@ def zad3():
             print(" " * (n - i), "A" * (i * 2 + 1))
 
 
-def zad4():
+def zad5():
     lista = []
     suma = 0
     n = int(input("Podaj n: "))
@@ -46,7 +46,7 @@ def main():
     # zad1()
     # zad2()
     # zad3()
-    zad4()
+    zad5()
 
 
 main()
