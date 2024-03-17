df18cd773ff24fdeb8f70b2bf3622a33a4344de2
AgataB3 <147510880+AgataB3@users.noreply.github.com>
2024-03-17 14:40:57 +0100

    Update wd3_zadania_lab3.py

--- wd3_zadania_lab3.py
+++ wd3_zadania_lab3.py
@@ -1,10 +1,11 @@
 import random
+import math
 
 
 def zad1():
-    A = [1-x for x in range(1, 11)]
+    A = [1 - x for x in range(1, 11)]
     print(A)
-    B = [4**i for i in range(8)]
+    B = [4 ** i for i in range(8)]
     print(B)
     C = [x for x in B if x % 2 == 0]
     print(C)
@@ -20,16 +21,66 @@ def zad2():
 
 
 def zad3():
-    produkty = {"Awokado": "sztuki",
-                "Orzechy włoskie": "kg",
-                "Banany": "kg",
-                "Pomidory w puszce": "sztuki"}
+    p = {'Awokado': "sztuki",
+         'Orzechy włoskie': "kg",
+         'Banany': "kg",
+         'Pomidory w puszce': "sztuki"}
+    lista = [x for x in p.keys() if p[x] == "sztuki"]
+    print(lista)
 
-    
+
+def zad4():
+    a = int(input("Podaj pierwszy bok trojkata: "))
+    b = int(input("Podaj drugi bok trojkata: "))
+    c = int(input("Podaj trzeci bok trojkata: "))
+    if (a**2 == b**2 + c**2) | (b**2 == a**2 + c**2) | (c**2 == a**2 + b**2):
+        print("Trojkat jest prostokatny.")
+    else:
+        print("Trojkat nie jest prostokatny.")
+
+
+def pole_trapezu(a=10, b=5, h=5):
+    return 1/2 * (a+b) * h
+
+
+def zad5():
+    a = int(input("Podaj dlugosc pierwszej podstawy: "))
+    b = int(input("Podaj dlugosc drugiej podstawy: "))
+    h = int(input("Podaj dlugosc wysokosci: "))
+    print(f'Pole trapezu: {pole_trapezu(a, b, h)}')
+
+
+def ciag(a1=1, b=4, ile=10):
+    iloczyn = 1
+    for i in range(ile):
+        iloczyn *= a1
+        a1 *= b
+    return iloczyn
+
+
+def zad6():
+    a1 = int(input("Podaj wartosc poczatkowa: "))
+    b = int(input("Podaj wielkosc o ile mnozone sa kolejne elementy: "))
+    ile = int(input("Podaj ile elementow mnozyc: "))
+    print(ciag(a1, b, ile))
+
+
+def zad7():
+    x = int(input("Podaj liczbe: "))
+    try:
+        print(math.sqrt(x))
+    except ValueError:
+        print("Nie mozna pierwiastkowac ujemnej liczby.")
+
+
 def main():
     # zad1()
     # zad2()
-    zad3()
+    # zad3()
+    # zad4()
+    # zad5()
+    # zad6()
+    zad7()
 
 
 main()
