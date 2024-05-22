f719a93675be73b49c56479ece39da3380a39d3f
AgataB3 <147510880+AgataB3@users.noreply.github.com>
2024-05-22 21:45:51 +0200

    Update wd5_numpy.py

--- wd5_numpy.py
+++ wd5_numpy.py
@@ -1,13 +1,14 @@
 import numpy as np
+import math
 
-print("Zad1:")
+print("\nZad1: ")
 a = np.array([3, 6, 9])
 b = np.array([1, 4, 8])
 print(a)
 print(b)
-print(a*b)
+print(a * b)
 
-print("\nZad2:")
+print("\nZad2: ")
 a = np.array([[3, 6, 9],
               [4, 7, 9],
               [2, 1, 7]])
@@ -21,4 +22,66 @@ print(f'Wartosci minimalne dla kolumn macierzy b: {b.min(axis=0)}')
 print(f'Wartosci minimalne dla wierszy macierzy a: {a.min(axis=1)}')
 print(f'Wartosci minimalne dla wierszy macierzy b: {b.min(axis=1)}')
 
-print("\nZad3:")
+print("\nZad3: ")
+c = a.dot(b)
+print(c)
+
+print("\nZad4: ")
+a = np.array([[1, 2, 7],
+               [7, 3, 9],
+               [6, 2, 8]])
+print(a)
+print(a.dtype)
+b = np.array([[5.7, 2, math.pi],
+               [9.1, 2.8, 8],
+               [math.e, 2.9, 8.3]])
+print(b)
+print(b.dtype)
+c = a * b
+print(c)
+print(c.dtype)
+
+print("\nZad5: ")
+a = np.array([[math.pi/2, 5, 1],
+              [3*math.pi/2, 4, 0]])
+b = np.sin(a)
+print(b)
+
+print("\nZad6: ")
+a = np.array([[4, math.pi, 1],
+              [0, 2, 2*math.pi]])
+b = np.cos(a)
+print(b)
+
+print("\nZad8: ")
+a = np.arange(9).reshape((3, 3))
+print(a)
+for x in a:
+    print(x)
+
+print("\nZad9: ")
+a = np.arange(9).reshape((3, 3))
+for x in a.flat:
+    print(x)
+
+print("\nZad10: ")
+a = np.arange(81).reshape((9, 9))
+print(a)
+a = a.reshape((3, 27)) #dzielniki liczby 81
+print(a)
+
+print("\nZad11: ")
+a = np.arange(12)
+print(a)
+a = a.reshape((3, 4))
+print(a)
+b = a.reshape((4, 3))
+print(b)
+c = a.reshape((2, 6))
+print(c)
+a = a.ravel()
+b = b.ravel()
+c = c.ravel()
+print(a)
+print(b)
+print(c)
